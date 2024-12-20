ad646faa7a6d90ee8f757d52168ac46c3c308739
Michał Mikuta <mmikuta@ethz.ch>
2024-12-20 14:33:06 +0100

    Cub concepts added

---
+++ experiments/data/CUB_setup.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+PATH = "CUB_200_2011/attributes/image_attribute_labels.txt"
+
+# read csv
+df = pd.read_csv(
+    PATH,
+    sep=r"\s+",
+    names=["image_id", "attribute_id", "is_present", "certainty_id", "time"],
+    error_bad_lines=False,
+)
+
+# reshape
+data = df.pivot_table(index="image_id", columns="attribute_id", values="is_present")
+
+data.to_csv("CUB_200_2011_concepts.csv")
