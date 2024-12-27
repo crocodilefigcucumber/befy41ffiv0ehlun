e4b9b7d8753f44d87335a8ac262c330b9129242f
Michał Mikuta <mmikuta@ethz.ch>
2024-12-27 18:03:05 +0100

    Clustering on CUB done

--- experiments/CUB_clusters.py
+++ experiments/CUB_clusters.py
@@ -4,10 +4,10 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import pdist
 import matplotlib.pyplot as plt
 import radialtree as rt  # https://github.com/koonimaru/radialtree
+from clustpy.hierarchical import Diana
 
-
-CONCEPT_PATH = "data/CUB_200_2011_concepts.csv"
-concepts = pd.read_csv(CONCEPT_PATH, index_col="image_id")
+TRAIN_CONCEPT_PATH = "data/CUB_200_2011_concepts_train.csv"
+concepts = pd.read_csv(TRAIN_CONCEPT_PATH, index_col="image_id")
 concepts = np.array(concepts)
 
 ATTRIBUTE_PATH = "data/CUB_200_2011/attributes/attributes.txt"
@@ -29,6 +29,7 @@ distance_matrix = pdist(concepts.T, metric=metric)
 # Apply agglomerative clustering using complete linkage
 linkage_matrix = linkage(distance_matrix, method="complete")
 
+"""
 no_concepts = concepts.shape[1]
 trivial = list(range(1, no_concepts + 1))
 
@@ -64,3 +65,34 @@ out = dendrogram(
 )
 rt.plot(out)
 plt.savefig(PLOTS_PATH + "collapsed.pdf", dpi=300)
+"""
+
+# Apply divisive clustering using same metric
+
+diana = Diana(metric=metric)
+diana.fit(concepts.T)
+no_clusters = 4
+clusters = diana.flat_clustering(n_leaf_nodes_to_keep=no_clusters)
+
+clust_str_labels = {label: [] for label in range(no_clusters)}
+clust_labels = {label: [] for label in range(no_clusters)}
+
+for i in range(len(clusters)):
+    clust_str_labels[clusters[i]].append(label_dict[i + 1])
+    clust_labels[clusters[i]].append(i)
+
+data = clust_labels
+max_len = max(len(v) for v in data.values())  # Find the longest list
+table_data = {
+    key: value + [""] * (max_len - len(value)) for key, value in data.items()
+}  # Pad shorter lists
+table_data = pd.DataFrame.from_dict(table_data, orient="columns")
+table_data.to_csv("clusters_idx.csv", index=False)
+
+data = clust_str_labels
+max_len = max(len(v) for v in data.values())  # Find the longest list
+table_data = {
+    key: value + [""] * (max_len - len(value)) for key, value in data.items()
+}  # Pad shorter lists
+table_data = pd.DataFrame.from_dict(table_data, orient="columns")
+table_data.to_csv("clusters_str.csv", index=False)
