46c8ba23046cd521e5d9179909127109702a1d9e
Michał Mikuta <mmikuta@ethz.ch>
2024-12-27 19:36:59 +0100

    wrapped as function

--- experiments/CUB_clusters.py
+++ experiments/CUB_clusters.py
@@ -3,8 +3,9 @@ import numpy as np
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import pdist
 import matplotlib.pyplot as plt
-import radialtree as rt  # https://github.com/koonimaru/radialtree
-from clustpy.hierarchical import Diana
+
+# import radialtree as rt  # https://github.com/koonimaru/radialtree
+from clustering import clusterConcepts
 
 TRAIN_CONCEPT_PATH = "data/CUB_200_2011_concepts_train.csv"
 concepts = pd.read_csv(TRAIN_CONCEPT_PATH, index_col="image_id")
@@ -69,32 +70,34 @@ plt.savefig(PLOTS_PATH + "collapsed.pdf", dpi=300)
 
 # Apply divisive clustering using same metric
 
-diana = Diana(metric=metric)
-diana.fit(concepts.T)
+# diana = Diana(metric=metric)
+# diana.fit(concepts.T)
 no_clusters = 4
-clusters = diana.flat_clustering(n_leaf_nodes_to_keep=no_clusters)
+# clusters = diana.flat_clustering(n_leaf_nodes_to_keep=no_clusters)
 
-clust_str_labels = {label: [] for label in range(no_clusters)}
-clust_labels = {label: [] for label in range(no_clusters)}
+# clust_str_labels = {label: [] for label in range(no_clusters)}
+# clust_labels = {label: [] for label in range(no_clusters)}
 
-for i in range(len(clusters)):
-    clust_str_labels[clusters[i]].append(label_dict[i + 1])
-    clust_labels[clusters[i]].append(i)
+# for i in range(len(clusters)):
+#     clust_str_labels[clusters[i]].append(label_dict[i + 1])
+#     clust_labels[clusters[i]].append(i)
 
-data = clust_labels
-max_len = max(len(v) for v in data.values())  # Find the longest list
-table_data = {
-    key: value + [""] * (max_len - len(value)) for key, value in data.items()
-}  # Pad shorter lists
-table_data = pd.DataFrame.from_dict(table_data, orient="columns")
-table_data.columns = [f"Cluster {label}" for label in table_data.columns]
+# data = clust_labels
+# max_len = max(len(v) for v in data.values())  # Find the longest list
+# table_data = {
+#     key: value + [""] * (max_len - len(value)) for key, value in data.items()
+# }  # Pad shorter lists
+# table_data = pd.DataFrame.from_dict(table_data, orient="columns")
+# table_data.columns = [f"Cluster {label}" for label in table_data.columns]
+table_data = clusterConcepts(concepts, no_clusters=no_clusters)
 table_data.to_csv("clusters_idx.csv", index=False)
 
-data = clust_str_labels
-max_len = max(len(v) for v in data.values())  # Find the longest list
-table_data = {
-    key: value + [""] * (max_len - len(value)) for key, value in data.items()
-}  # Pad shorter lists
-table_data = pd.DataFrame.from_dict(table_data, orient="columns")
-table_data.columns = [f"Cluster {label}" for label in table_data.columns]
+# data = clust_str_labels
+# max_len = max(len(v) for v in data.values())  # Find the longest list
+# table_data = {
+#     key: value + [""] * (max_len - len(value)) for key, value in data.items()
+# }  # Pad shorter lists
+# table_data = pd.DataFrame.from_dict(table_data, orient="columns")
+# table_data.columns = [f"Cluster {label}" for label in table_data.columns]
+table_data = clusterConcepts(concepts, no_clusters=no_clusters, label_dict=labels)
 table_data.to_csv("clusters_str.csv", index=False)
--- experiments/clustering.py
+++ experiments/clustering.py
@@ -1,69 +1,35 @@
-import numpy as np
-from scipy.special import softmax
+from pandas import DataFrame
+from clustpy.hierarchical import Diana
 
 
-"""
-CODE TAKEN FROM arxiv:2410.07858 APPENDIX A
-Copyright (authors, by listing in paper):
-Emanuele Palumbo
-Moritz Vandenhirtz
-Alain Ryser
-Imant Daunhawer
-Julia E. Vogt
-"""
+def clusterConcepts(concepts, no_clusters, label_dict=None, metric="jaccard"):
+    """Perform DIANA clustering on concepts, export as DataFrame
 
-
-def L2H(logits):
+    Keyword arguments:
+    concepts -- np.array of shape (N,n_concepts)
+    label_dict -- only pass if you want string outputs
     """
-    L2H Algorithm.
-    Args:
-    logits: Logits from model (N x K) where N number of datapoints in the dataset
-    and K is the number of clusters
-    Returns:
-    steps: Merging steps characterizing the hierarchy
-    """
-    # Number of clusters is equal to size of last dimension in the logits
-    K = logits.shape[-1]
-    # Initialize groups of clusters to single clusters
-    groups = [(c,) for c in range(K)]
-    # Initialize list of steps that characterize hierarchy
-    steps = []
-    # Given the logits for the whole dataset, compute assignments and predicted probabilities
-    softmaxed_logits = softmax(logits, axis=-1)
-    assignments = np.argmax(softmaxed_logits, axis=-1)
-    pred_probs = np.max(softmaxed_logits, axis=-1)
-    for step in range(1, K):
-        # Compute score for for each group (which chosen aggregation function)
-        score_per_gr = {}
-        for group in groups:
-            score_per_gr[group] = sum(
-                [np.mean(pred_probs[assignments == c]) for c in group]
-            )
-        # Get the group with the lowest score (lsg), will be merged at this iteration
-        lsg = min(score_per_gr, key=score_per_gr.get)
-        # Get the logits for datapoints assigned to the lowest score group
-        logits_lsg = logits[np.where(np.isin(assignments, lsg))[0]]
-        # Reassign datapoints in lsg to only clusters not in lsg,
-        # and re-compute predicted probabilities
-        msm_logits_lsg = np.zeros_like(logits_lsg)
-        cls_not_in_lsg = [c for c in range(K) if c not in lsg]
-        cls_in_lsg = [c for c in range(K) if c in lsg]
-        msm_logits_lsg[:, cls_not_in_lsg] = softmax(
-            logits_lsg[:, cls_not_in_lsg], axis=-1
-        )
-        msm_logits_lsg[:, cls_in_lsg] = 0.0
-        reassignments = np.argmax(msm_logits_lsg, axis=-1)
-        re_pred_probs = np.max(msm_logits_lsg, axis=-1)
-        # Compute the total reassigned predicted probability per cluster and average across
-        # clusters in each group.Then select the group with the highest average.
-        re_pp_per_group = {
-            group: np.mean([np.sum(re_pred_probs[reassignments == c]) for c in group])
-            for group in groups
-            if group != lsg
-        }
-        mtg = max(re_pp_per_group, key=re_pp_per_group.get)
-        # Merge `lsg` with `mtg` and update `groups`.
-        groups = [gr for gr in groups if gr not in [lsg, mtg]] + [lsg + mtg]
-        # Add merging in current iteration to steps
-        steps.append((lsg, mtg))
-    return steps
+
+    diana = Diana(metric=metric)
+    diana.fit(concepts.T)
+    clusters = diana.flat_clustering(n_leaf_nodes_to_keep=no_clusters)
+
+    if label_dict is not None:
+        clust_str_labels = {label: [] for label in range(no_clusters)}
+
+        for i in range(len(clusters)):
+            clust_str_labels[clusters[i]].append(label_dict[i])
+
+        data = clust_str_labels
+    else:
+        clust_labels = {label: [] for label in range(no_clusters)}
+        for i in range(len(clusters)):
+            clust_labels[clusters[i]].append(i)
+
+        data = clust_labels
+
+    max_len = max(len(v) for v in data.values())  # Find the longest list
+    table_data = {
+        key: value + [""] * (max_len - len(value)) for key, value in data.items()
+    }  # Pad shorter lists
+    return DataFrame.from_dict(table_data, orient="columns")
