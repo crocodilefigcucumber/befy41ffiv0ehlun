d852b9447c54dfcd4706061743b44f3dcae7f569
Michał Mikuta <mmikuta@ethz.ch>
2025-01-15 14:12:49 +0100

    Added Baseline to maxinter plot

--- realignment/maxinter_visualize.py
+++ realignment/maxinter_visualize.py
@@ -15,10 +15,17 @@ def main():
     # Set a Seaborn theme
     sns.set_theme(context="paper", style="whitegrid", palette="colorblind")
 
+    # We call 'Baseline' UCP in the paper
+    df.loc[df["model_type"] == "Baseline", "model_type"] = "UCP"
+
+
     # Create a line plot
     sns.lineplot(
         data=df, x="max_interventions", y="test_acc", hue="model_type", marker="o",linewidth=4
     )
+
+    # Add end-to-end accuracy as "Baseline" in the plot
+    plt.axhline(y=26.37,color="black", linestyle="--", linewidth=2, label="Baseline")
 
     # Label axes and add a title
     plt.xlabel("Maximum Interventions")
